e96a57b990b518d7d19931a30e706e8c044fd556
myname21-meet <myname21@meet.mit.edu>
2019-08-07 09:55:32 +0300

    2

--- static/js/search_bar.js
+++ static/js/search_bar.js
@@ -2,7 +2,7 @@ function myFunction() {
 	var input, filter, ul, li, a, i, txtValue;
 	input = $('#searchInput'):
 	filter = input.val().toUpperCase();
-	ul = $("#sroreList");
+	ul = $("#storeList");
 	li = $(".store_info").each(function(){
 		txtValue = $(this).text().toUpperCase();
 		if (txtValue.includes(filter)) {
